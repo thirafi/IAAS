d3bc0ac12eff03657f86c9eab459bd8e91d1bc00
sinduwinarta <sindu.winarta@gmail.com>
2018-12-19 20:59:25 +0700

    ini sidebar admin
    admin

---
+++ frontend/user/src/app/elements/sidebaar/sidebaar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebaar',
+  templateUrl: './sidebaar.component.html',
+  styleUrls: ['./sidebaar.component.css']
+})
+export class SidebaarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
